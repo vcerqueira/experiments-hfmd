8497da083e42867838ead2e81b561cb93ae3bc3e
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-10-19 18:03:36 +0100

    src

--- scripts/experiments/run/run_classical.py
+++ scripts/experiments/run/run_classical.py
@@ -19,15 +19,15 @@ df = DataReader.load_data(drop_exogenous=True)
 
 models = [
     RandomWalkWithDrift(),
-    # SeasonalNaive(season_length=PERIOD),
-    # AutoTheta(season_length=PERIOD),
+    SeasonalNaive(season_length=PERIOD),
+    AutoTheta(season_length=PERIOD),
     ARIMA(season_length=PERIOD, order=(5, 1, 1)),
-    # SimpleExponentialSmoothingOptimized(),
-    # AutoETS(),
-    # ADIDA(),
-    # CrostonOptimized(),
-    # IMAPA(),
-    # TSB(alpha_d=0.2, alpha_p=0.2),
+    SimpleExponentialSmoothingOptimized(),
+    AutoETS(),
+    ADIDA(),
+    CrostonOptimized(),
+    IMAPA(),
+    TSB(alpha_d=0.2, alpha_p=0.2),
 ]
 
 sf = StatsForecast(
@@ -40,4 +40,4 @@ cv = sf.cross_validation(df=df, h=HORIZON, n_windows=TEST_SIZE)
 
 cv = cv.reset_index()
 
-cv.to_csv(f'/Users/vcerq/Desktop/cv_hfmd_cls2.csv', index=False)
+cv.to_csv(f'{RESULTS_FILE_PATH}/cv_hfmd_cls.csv', index=False)
