0ce2d833768362a223870310ebf13129db436b13
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-10-20 11:04:57 +0100

    eda

--- scripts/experiments/analysis/0_eda_plots.py
+++ scripts/experiments/analysis/0_eda_plots.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import plotnine as p9
 
 from src.data_reader import DataReader
@@ -5,15 +7,17 @@ from config import PLOT_THEME
 
 df = DataReader.load_data()
 
-df = df.drop(['unique_id', 'ds'], axis=1)
+df_ = df.drop(['unique_id', 'ds'], axis=1)
 
-corr_df = df.corr()
+corr_df = df_.corr()
 # corr_df = df.corr(method='kendall')
 
 dfm = corr_df.reset_index().melt('index')
 dfm['index'] = dfm['index'].apply(lambda x: x.capitalize()).replace({'Y': 'HMFD'})
 dfm['variable'] = dfm['variable'].apply(lambda x: x.capitalize()).replace({'Y': 'HMFD'})
 dfm.columns = ['v1', 'v2', 'Correlation']
+
+# correlation heatmap
 
 plot = \
     p9.ggplot(data=dfm,
@@ -26,3 +30,32 @@ plot = \
     p9.scale_fill_gradient2(low='red', mid='white', high='blue')
 
 plot.save('assets/outputs/plot_eda_corr.pdf', width=6, height=6)
+
+#
+
+df_uv = df.drop(columns=['temperature', 'rainfall', 'humidity'])
+df_uv_m = df_uv.groupby('unique_id').resample('MS', on='ds').agg({'y': 'sum'}).reset_index()
+
+plot_uv = p9.ggplot(data=df_uv_m,
+                    mapping=p9.aes(x='ds',
+                                   y='np.log(y+1)',
+                                   color='unique_id')) + \
+          p9.geom_line() + \
+          PLOT_THEME + \
+          p9.labs(x='', y='HMFD', color='Location') + \
+          p9.theme(legend_position='right')
+
+plot_uv.save('assets/outputs/plot_eda_uv_monthly.pdf', width=12, height=6)
+
+df_uv_sub = df_uv[df_uv['ds'] < '2009-07-01']
+
+plot_uv = p9.ggplot(data=df_uv_sub,
+                    mapping=p9.aes(x='ds',
+                                   y='np.log(y+1)',
+                                   color='unique_id')) + \
+          p9.geom_line() + \
+          PLOT_THEME + \
+          p9.labs(x='', y='HMFD', color='Location') + \
+          p9.theme(legend_position='right')
+
+plot_uv.save('assets/outputs/plot_eda_uv.pdf', width=12, height=6)
