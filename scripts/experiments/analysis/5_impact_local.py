f9c60237b1c4708326e50f73f132c53efabaf2f0
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2025-10-24 09:49:06 +0100

    local exp

--- scripts/experiments/analysis/5_impact_local.py
+++ scripts/experiments/analysis/5_impact_local.py
@@ -8,21 +8,21 @@ from neuralforecast.losses.numpy import smape
 from config import PLOT_THEME
 
 cv_uv = pd.read_csv('assets/results/cv.csv')
-cv_exog = pd.read_csv('assets/results/cv_hfmd_nf_poisson_exog.csv')
+cv_loc = pd.read_csv('assets/results/cv_hfmd_nf_local.csv')
 
 MODEL_NAMES = ['AutoKAN-P', 'AutoMLP-P', 'AutoNHITS-P', 'AutoPatchTST-P', 'AutoLSTM-P']
-MODEL_NAMES_EX = ['AutoKAN-median', 'AutoMLP-median', 'AutoNHITS-median', 'AutoLSTM-median']
+MODEL_NAMES_LOC = ['AutoKAN-median', 'AutoMLP-median', 'AutoNHITS-median', 'AutoPatchTST-median', 'AutoLSTM-median']
 META_COLS = ['unique_id', 'ds', 'cutoff', 'y']
 
 cv_uv = cv_uv[META_COLS + MODEL_NAMES]
 # cv = cv.rename(columns={col: col.replace('-P', '') for col in cv.columns})
-cv_exog = cv_exog[META_COLS + MODEL_NAMES_EX]
-cv_exog = cv_exog.rename(columns={col: col.replace('-median', '') for col in cv_exog.columns})
+cv_loc = cv_loc[META_COLS + MODEL_NAMES_LOC]
+cv_loc = cv_loc.rename(columns={col: col.replace('-median', '') for col in cv_loc.columns})
 
 pprint(cv_uv.columns.tolist())
-pprint(cv_exog.columns.tolist())
+pprint(cv_loc.columns.tolist())
 
-cv = cv_uv.merge(cv_exog.drop(columns=['y']), on=['unique_id', 'ds', 'cutoff'])
+cv = cv_uv.merge(cv_loc.drop(columns=['y']), on=['unique_id', 'ds', 'cutoff'])
 pprint(cv.columns.tolist())
 
 all_models = [c for c in cv.columns if c not in META_COLS]
@@ -41,12 +41,11 @@ for g, df in cv_group:
 results_df = pd.DataFrame(results_by_series).T
 
 err = results_df.mean()
-err['AutoPatchTST'] = np.nan
 
 err_df = err.reset_index()
 err_df.columns = ['model', 'value']
 
-err_df['type'] = err_df['model'].apply(lambda x: 'Univariate' if x.endswith('-P') else 'With exogenous')
+err_df['type'] = err_df['model'].apply(lambda x: 'Global' if x.endswith('-P') else 'Local')
 err_df['model'] = err_df['model'].apply(lambda x: x.replace('-P', ''))
 
 err_df.columns = ['Model', 'SMAPE', 'Approach']
@@ -62,4 +61,4 @@ plot = p9.ggplot(err_df, mapping=p9.aes(x='Model',
        PLOT_THEME + \
        p9.theme(axis_text=p9.element_text(size=13))
 
-plot.save('assets/outputs/plot_smape_exog.pdf', width=11, height=7)
+plot.save('assets/outputs/plot_smape_local.pdf', width=11, height=7)
